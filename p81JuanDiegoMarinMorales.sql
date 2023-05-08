d61b65176f72d5d669220b7a96fe310ba0dcd007
JuanDiego <jmarmor2111@g.educaan.es>
2023-05-08 14:42:04 +0200

    basedatis

---
+++ p81JuanDiegoMarinMorales.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE  IF NOT EXISTS `p81JuanDiegoMarinMorales`;
+USE `p81JuanDiegoMarinMorales`;
+
+
+
+DROP TABLE IF EXISTS `factura`;
+
+CREATE TABLE `factura` (
+  `pk` int(11) NOT NULL,
+  `fecha_emision` datetime DEFAULT NULL,
+  `descripcion` varchar(45) DEFAULT NULL,
+  `totalImporte` decimal(6,2),
+  PRIMARY KEY (`pk`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
