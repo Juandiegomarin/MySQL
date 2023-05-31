25b4e867b0f3c398bbaa0698736cc796c52bade0
JuanDiego <jmarmor2111@g.educaan.es>
2023-05-31 12:29:37 +0200

    f

--- proyectoFinal.sql
+++ proyectoFinal.sql
@@ -67,16 +67,17 @@ insert into generacion
  
  insert into pokemon
  values
- (1,6,1,'Charizard','Fuego','Volador'),
+ (3,6,1,'Charizard','Fuego','Volador'),
  (2,25,1,'Pikachu','Electrico',default);
  
  insert into habilidad
  values
- (default,'Mar LLamas','Aumenta los ataques de tipo fuego cuando el poseedor tiene menos de 1/3 de la vida'),
- (default,'Absorbe electricidad','Cuando el poseedor recibe un ataque de tipo electrico le aumenta el Sp.Atk por 1,5'),
- (default,'Poder Solar','Poder solar con sol o sol abrasador aumenta el ataque especial del Pokémon poseedor en un 50%, pero a cambio el Pokémon pierde 1/8 de sus PS máximos al final de cada turno.');
+ (1,'Mar LLamas','Aumenta los ataques de tipo fuego cuando el poseedor tiene menos de 1/3 de la vida'),
+ (2,'Absorbe electricidad','Cuando el poseedor recibe un ataque de tipo electrico le aumenta el Sp.Atk por 1,5'),
+ (3,'Poder Solar','Poder solar con sol o sol abrasador aumenta el ataque especial del Pokémon poseedor en un 50%, pero a cambio el Pokémon pierde 1/8 de sus PS máximos al final de cada turno.');
  
  insert into habilidadPokemon
+ (idHabilidad,idPokemon,oculta)
  values
  (1,1,0),
  (3,3,1),
